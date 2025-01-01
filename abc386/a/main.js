072da55819bfdb7a9f971da734550feb550626d9
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-01 15:12:18 +0900

    解説を見た後解いた

--- abc386/a/main.js
+++ abc386/a/main.js
@@ -1,6 +1,24 @@
 function Main(input) {
     input = input.split("\n");
-    console.log(input);
+    const cards = input[0].split(" ");
+
+    // 昇順に並び替える.
+    function sortCards(a,b){
+        return a - b;
+    }
+    cards.sort(sortCards);
+
+    if(cards[0] != cards[1] && cards[1] == cards[2] && cards[2] == cards[3]){
+        console.log("Yes");
+    }
+    else if(cards[0] == cards[1] && cards[1] != cards[2] && cards[2] == cards[3]){
+        console.log("Yes");
+    }else if(cards[0] == cards[1] && cards[1] == cards[2] && cards[2] != cards[3]){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
