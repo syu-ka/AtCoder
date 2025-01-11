52594571a76f36a28b793301c94b1738b64ba77f
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-12 03:34:40 +0900

    余計なところ削除・コメント追加

--- abc388/c/main.js
+++ abc388/c/main.js
@@ -1,48 +1,42 @@
 function Main(input) {
-    const N = Number(input[0]);
-    const A = input[1].split(" ").map(Number);
-    // console.log(A);
+    const N = Number(input[0]); // N個の餅.
+    const A = input[1].split(" ").map(Number); // A_i: i番目の餅の大きさ.A_i<=A_{i+1}.
 
-    let count = 0;
+    let count = 0; // 何種類の鏡餅が作れるかカウント.
 
-    let key;
+    // 2分探索法のための変数.
+    let key; // 下の餅になり得る最小値. つまり 上の餅の２倍の重さ.
     let searchLeft = 0;
     let searchRight = A.length - 1;
     let half;
 
-    for (let i = 0; i < N; i++) {
+    for (let i = 0; i < N; i++) { //上の餅の重さがA[i].
         key = A[i] * 2;
-        // console.log("--------------");
-        // console.log(`key=${key}`);
+
+        // 下の餅になり得る餅が１つもない.
         if (key > A[A.length - 1]) {
+            // i++なったときにkeyが現在より小さくなることがないのでcontinueではなくbreak.
             break;
         }
+
         searchLeft = 0;
         searchRight = A.length - 1;
-
-        // console.log(searchLeft, searchRight, key, half);
 
         // 2文探索法で探す.
         while (searchLeft < searchRight) {
             half = Math.floor((searchRight + searchLeft) / 2);
-            // console.log(`half=${half}`);
-            // console.log(A[half]);
-            // console.log(`searchRight= ${searchRight}, searchLeft= ${searchLeft}`);
-            // console.log(`half= ${half}`);
 
             if (key <= A[half]) {
                 searchRight = half;
-                // console.log(`searchRight= ${searchRight}`);
             } else if (key > A[half]) {
                 searchLeft = half + 1;
-                // console.log(`searchLeft= ${searchLeft}`);
             }
         }
-        // console.log(`count += ${A.length - searchLeft}`);
+
+        // A[searchLeft]以降の餅は鏡餅になり得るためカウント.
         count += A.length - searchLeft;
     }
     console.log(count);
-    // console.log(`count=${count}`);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
