d2f28156220055581e87149d8de5f194e4c58590
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-12 00:09:02 +0900

    c 初AC

--- abc388/b/main.js
+++ abc388/b/main.js
@@ -1,6 +1,25 @@
 function Main(input) {
-    const test = input[0].split(" ");
-    console.log(test);
+    const [N, D] = input[0].split(" ").map(Number);
+    let T = []; //太さ.
+    let L = []; //長さ.
+    let W; //重さ.
+    let Wmax = []; //重さ.
+    for (let i = 0; i < N; i++) {
+        T.push(input[i + 1].split(" ").splice(0, 1).map(Number));
+        L.push(input[i + 1].split(" ").splice(1, 1).map(Number));
+    }
+
+    for (let k = 1; k <= D; k++) {
+        Wmax.push(0);
+        for (let i = 0; i < N; i++) {
+            W = T[i] * (Number(L[i]) + Number(k));
+            if (Wmax[Wmax.length - 1] < W) {
+                Wmax[Wmax.length - 1] = W;
+            }
+        }
+        console.log(Wmax[k - 1]);
+    }
+
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
--- abc388/c/main.js
+++ abc388/c/main.js
@@ -1,6 +1,48 @@
 function Main(input) {
-    const test = input[0].split(" ");
-    console.log(test);
+    const N = Number(input[0]);
+    const A = input[1].split(" ").map(Number);
+    // console.log(A);
+
+    let count = 0;
+
+    let key;
+    let searchLeft = 0;
+    let searchRight = A.length - 1;
+    let half;
+
+    for (let i = 0; i < N; i++) {
+        key = A[i] * 2;
+        // console.log("--------------");
+        // console.log(`key=${key}`);
+        if (key > A[A.length - 1]) {
+            break;
+        }
+        searchLeft = 0;
+        searchRight = A.length - 1;
+
+        // console.log(searchLeft, searchRight, key, half);
+
+        // 2文探索法で探す.
+        while (searchLeft < searchRight) {
+            half = Math.floor((searchRight + searchLeft) / 2);
+            // console.log(`half=${half}`);
+            // console.log(A[half]);
+            // console.log(`searchRight= ${searchRight}, searchLeft= ${searchLeft}`);
+            // console.log(`half= ${half}`);
+
+            if (key <= A[half]) {
+                searchRight = half;
+                // console.log(`searchRight= ${searchRight}`);
+            } else if (key > A[half]) {
+                searchLeft = half + 1;
+                // console.log(`searchLeft= ${searchLeft}`);
+            }
+        }
+        // console.log(`count += ${A.length - searchLeft}`);
+        count += A.length - searchLeft;
+    }
+    console.log(count);
+    // console.log(`count=${count}`);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
