a30066e4a7c2a892fe90e5e91a84b6165a55b433
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-14 13:50:17 +0900

    初AC

--- abs/abc081b/main.js
+++ abs/abc081b/main.js
@@ -1,6 +1,20 @@
 function Main(input) {
-    const test = input[0].split(" ");
-    console.log(test);
+    const N = Number(input[0]);
+    const A = input[1].split(" ").map(Number);
+    let count = 0;
+    let isTrue = true;
+    while (isTrue) {
+        for (let i = 0; i < N; i++) {
+            if (A[i] % 2 !== 0) {
+                isTrue = false;
+                break;
+            }
+            A[i] /= 2;
+        }
+        if (isTrue) count++;
+
+    }
+    console.log(count);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
