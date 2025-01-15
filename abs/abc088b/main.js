160d1c03bff71c1aaec5da6b98275c2be17bfd44
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-16 00:58:22 +0900

    初AC

--- abs/abc088b/main.js
+++ abs/abc088b/main.js
@@ -1,6 +1,20 @@
 function Main(input) {
-    const test = input[0].split(" ");
-    console.log(test);
+    const N = Number(input[0]);
+    const a = input[1].split(" ").map(Number);
+    a.sort((a, b) => {
+        return b - a;
+    });
+
+    let aliceSum = 0;
+    let bobSum = 0;
+    for (let i = 0; i < N; i++) {
+        if (i % 2 === 0) {
+            aliceSum += a[i];
+        } else {
+            bobSum += a[i];
+        }
+    }
+    console.log(aliceSum - bobSum);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
