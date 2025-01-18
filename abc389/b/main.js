d9d40ce6d137c188f09fdf416f8d56631b7907ca
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-18 21:11:44 +0900

    初AC

--- abc389/b/main.js
+++ abc389/b/main.js
@@ -1,6 +1,12 @@
 function Main(input) {
-    const test = input[0].split(" ");
-    console.log(test);
+    const X = Number(input[0]);
+    let N = 1;
+    let Nfactorial = 1;
+    while (Nfactorial != X) {
+        N++;
+        Nfactorial *= N;
+    }
+    console.log(N);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
