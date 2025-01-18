2ec9f2ae8315341c9a998abafbd4a99bfb8cd23e
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-18 21:04:00 +0900

    初AC

--- abc389/a/main.js
+++ abc389/a/main.js
@@ -1,6 +1,7 @@
 function Main(input) {
-    const test = input[0].split(" ");
-    console.log(test);
+    const S = input[0].split("");
+
+    console.log(Number(S[0]) * Number(S[2]));
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
