9cfeca29c1ce83335aa9570b0ba647d6439506ce
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-19 10:15:05 +0900

    説明加えた

--- abc389/c/main.js
+++ abc389/c/main.js
@@ -1,33 +1,31 @@
 function Main(input) {
     const Q = Number(input[0]);
-    let kind = [];
-    let detail = [];
-    let kind1count = 0;
-    let kind2count = 0;
-    let kind3flag = false;
-    let snakeQueue = [];
-    let snakeHeadQueue = [0];
-    let kind2sum = 0;
+    let type = []; //クエリのタイプ. 1<=type<=3.
+    let detail = []; //クエリの詳細.type==1なら長さl,type==3ならk番目.
+    let type1count = 0, type2count = 0; //それぞれtype1,2のクエリが実行された回数.
+    let type3flag = false; //1回でもtype3のクエリがあればtrue.
+    let snakeQueue = []; //最後尾にl(ヘビの長さ)を追加する用の配列.
+    let snakeHeadQueue = [0]; //最後尾にk番目のヘビの頭の座標を追加する用の配列.
+    let type2sum = 0; //type2で列から抜けたヘビの長さの合計.
     for (let i = 0; i < Q; i++) {
-        [kind[i], detail[i]] = input[i + 1].split(" ").map(Number);
+        [type[i], detail[i]] = input[i + 1].split(" ").map(Number);
 
-        if (kind[i] == 1) { //最後尾に追加.
-            kind1count++;
-            snakeQueue.push(detail[i]); //最後尾に追加.
+        if (type[i] == 1) { //最後尾に追加.
+            type1count++;
+            snakeQueue.push(detail[i]);
+            snakeHeadQueue.push(snakeHeadQueue[type1count - 1] + detail[i]);
 
-            snakeHeadQueue.push(snakeHeadQueue[kind1count - 1] + detail[i]);
-
-        } else if (kind[i] == 2) { //先頭を削除して詰める.
-            kind2count++;
-            kind2sum += snakeQueue[kind2count - 1];
-        } else if (kind[i] == 3) {
-            kind3flag = true;
-
-            console.log(snakeHeadQueue[kind2count + (detail[i] - 1)] - kind2sum);
+        } else if (type[i] == 2) { //先頭が列から抜ける.
+            type2count++;
+            type2sum += snakeQueue[type2count - 1];
+        } else if (type[i] == 3) {
+            type3flag = true;
+            const k = type2count + detail[i]; //k番目にいるヘビのindex.
+            console.log(snakeHeadQueue[k - 1] - type2sum);
         }
     }
 
-    if (!kind3flag) {
+    if (!type3flag) {
         console.log();
     }
 }
