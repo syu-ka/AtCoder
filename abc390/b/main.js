855add75046f5230e1093a6f985e2a46c47f301d
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-25 22:32:44 +0900

    初AC

--- abc390/b/main.js
+++ abc390/b/main.js
@@ -1,6 +1,19 @@
 function Main(input) {
-    const test = input[0].split(" ");
-    console.log(test);
+    const N = Number(input[0]);
+    const A = input[1].split(" ").map(Number);
+    const commonRatio = A[1] / A[0];
+    let isGeometricProgression = true;
+    for (let i = 1; i < A.length; i++) {
+        if (A[i] !== A[i - 1] * commonRatio) {
+            isGeometricProgression = false;
+        }
+    }
+    if (isGeometricProgression) {
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
+
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
