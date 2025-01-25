4e6ac29a9a85004f94013c97d455eb01974f2ef1
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-25 21:42:42 +0900

    初AC

--- abc390/a/main.js
+++ abc390/a/main.js
@@ -1,6 +1,26 @@
 function Main(input) {
-    const test = input[0].split(" ");
-    console.log(test);
+    const As = input[0].split(" ");
+    function sort(a, b) { return a - b; };
+    const AsSort = As.toSorted(sort);
+    // console.log(AsSort);
+
+    let isSwap = false;
+    for (let i = 0; i < As.length; i++) {
+        if (AsSort[i] !== As[i]) {
+            let temp = As[i];
+            As[i] = As[i + 1];
+            As[i + 1] = temp;
+            isSwap = true;
+            break;
+        }
+    }
+    let isMatched = true;
+    for (let i = 0; i < As.length; i++) {
+        if (AsSort[i] !== As[i]) {
+            isMatched = false;
+        }
+    }
+    (isMatched && isSwap) ? console.log("Yes") : console.log("No");
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
