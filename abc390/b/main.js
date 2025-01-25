a20782fd5035196a00205161be4c89a6bee9c344
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-26 00:09:11 +0900

    変数の説明追加、ifを三項演算子で書き換え

--- abc390/b/main.js
+++ abc390/b/main.js
@@ -1,18 +1,14 @@
 function Main(input) {
     const N = Number(input[0]);
     const A = input[1].split(" ").map(Number);
-    const commonRatio = A[1] / A[0];
-    let isGeometricProgression = true;
+    const commonRatio = A[1] / A[0]; //公比.
+    let isGeometricProgression = true; //等比数列か.
     for (let i = 1; i < A.length; i++) {
         if (A[i] !== A[i - 1] * commonRatio) {
             isGeometricProgression = false;
         }
     }
-    if (isGeometricProgression) {
-        console.log("Yes");
-    } else {
-        console.log("No");
-    }
+    isGeometricProgression ? console.log("Yes") : console.log("No");
 
 }
 
