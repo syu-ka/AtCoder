b5972ec4d4d135449b5341bcf36f6b0da376cb93
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-26 00:41:40 +0900

    初AC

--- abc390/c/main.js
+++ abc390/c/main.js
@@ -1,6 +1,33 @@
 function Main(input) {
-    const test = input[0].split(" ");
-    console.log(test);
+    let isPossible = true;
+    const [H, W] = input[0].split(" ").map(Number);
+    let S = [];
+    let a, b, c, d;
+    for (let i = 0; i < H; i++) {
+        S.push(input[i + 1].split(""));
+        for (let j = 0; j < W; j++) {
+            if (S[i][j] === "#") {
+                // console.log(S[i][j]);
+                // console.log(i, j);
+                if (a > i || a == undefined) a = i;
+                if (b < i || b == undefined) b = i;
+                if (c > j || c == undefined) c = j;
+                if (d < j || d == undefined) d = j;
+            }
+        }
+    }
+    // console.log(a, b, c, d);
+    for (let i = a; i <= b; i++) {
+        for (let j = c; j <= d; j++) {
+            if (S[i][j] === ".") isPossible = false;
+        }
+    }
+
+    if (isPossible) {
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
