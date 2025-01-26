62ecd24e976d3ea281d8689388802df8d29d0837
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-26 15:25:29 +0900

    解説視聴後（Setメゾットを使い重複のない集合を作成）

--- abs/abc085b/main.js
+++ abs/abc085b/main.js
@@ -1,16 +1,11 @@
 function Main(input) {
     const N = Number(input[0]);
     let d = [];
-    let kind = [];
     for (let i = 0; i < N; i++) {
         d.push(Number(input[i + 1]));
-        for (let j = -1; j < kind.length; j++) {
-            if (d[i] == kind[j]) break;
-            if (j == kind.length - 1) kind.push(d[i]);
-        }
     }
-
-    console.log(kind.length);
+    const set = new Set(d);
+    console.log(set.size); //lengthではなくsize.
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
