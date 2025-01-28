a4bfa2786c01770b40a9d947c6b7da6c9e1dede9
syu-ka <s3622021@campus.nias.ac.jp>
2025-01-28 13:55:26 +0900

    桁変更なし

--- abs/abc085c/main.js
+++ abs/abc085c/main.js
@@ -1,24 +1,18 @@
 function Main(input) {
     const [N, Y] = input[0].split(" ").map(Number);
-    const Ydiv1000 = Y / 1000;
-    // console.log(N, Y);
     const kind = [10000, 5000, 1000];
-    const kindDiv1000 = [10, 5, 1];
     let isPossible = false;
 
     for (let i = 0; i <= N; i++) {
-        // console.log(i);
         for (let j = 0; j <= i; j++) {
-            // console.log(i, j);
-            // console.log((N - i), j, (i - j));
-            let temp1 = (N - i) * kindDiv1000[0] + j * kindDiv1000[1] + (i - j) * kindDiv1000[2];
-            let temp2 = (N - i) * kindDiv1000[0] + (i - j) * kindDiv1000[1] + j * kindDiv1000[2];
+            let temp1 = (N - i) * kind[0] + j * kind[1] + (i - j) * kind[2];
+            let temp2 = (N - i) * kind[0] + (i - j) * kind[1] + j * kind[2];
 
-            if (temp1 == Ydiv1000) {
+            if (temp1 == Y) {
                 isPossible = true;
                 console.log((N - i), j, (i - j));
                 break;
-            } else if (temp2 == Ydiv1000) {
+            } else if (temp2 == Y) {
                 isPossible = true;
                 console.log((N - i), (i - j), j);
                 break;
