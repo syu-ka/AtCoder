e56fb8664090ba1821f68deb1fb2c05bf824e2a3
syu-ka <s3622021@campus.nias.ac.jp>
2025-02-01 21:36:15 +0900

    詰まっている途中

--- abc391/b/main.js
+++ abc391/b/main.js
@@ -1,8 +1,22 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [N, M] = input[0].split(" ").map(Number);
+    let S = [];
+    let T = [];
+
+    for (let i = 0; i < N; i++) {
+        S.push(input[i + 1].split(""));
+    }
+    for (let i = 0; i < M; i++) {
+        T.push(input[i + N + 1].split(""));
+    }
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; i++) {
+            if (T[0][0] == S[0][i])
+        }
+    }
+    console.log(N, M);
+    console.log(S);
+    console.log(T);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
