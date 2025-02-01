830f582035babed862dc7bb22f5192c4935a516b
syu-ka <s3622021@campus.nias.ac.jp>
2025-02-01 21:10:19 +0900

    初AC

--- abc391/a/main.js
+++ abc391/a/main.js
@@ -1,8 +1,43 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const D = input[0];
+    let answer;
+    switch (D) {
+        case "N":
+            answer = "S";
+            break;
+
+        case "E":
+            answer = "W";
+            break;
+
+        case "W":
+            answer = "E";
+            break;
+
+        case "S":
+            answer = "N";
+            break;
+
+        case "NE":
+            answer = "SW";
+            break;
+
+        case "NW":
+            answer = "SE";
+            break;
+
+        case "SE":
+            answer = "NW";
+            break;
+
+        case "SW":
+            answer = "NE";
+            break;
+
+        default:
+            break;
+    }
+    console.log(answer);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
