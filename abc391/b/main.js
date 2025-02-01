019334230ad724441dc930f58dbd92b30b20b84a
syu-ka <s3622021@campus.nias.ac.jp>
2025-02-01 23:33:35 +0900

    WA(初提出)

--- abc391/b/main.js
+++ abc391/b/main.js
@@ -1,7 +1,13 @@
 function Main(input) {
     const [N, M] = input[0].split(" ").map(Number);
-    let S = [];
-    let T = [];
+    let S = [""];
+    let T = [""];
+    let index1 = 1;
+    let index2 = 1;
+    let isTrue = false;
+    let isPrevTrue = false;
+    let trueCount = 0;
+    let a, b;
 
     for (let i = 0; i < N; i++) {
         S.push(input[i + 1].split(""));
@@ -9,14 +15,49 @@ function Main(input) {
     for (let i = 0; i < M; i++) {
         T.push(input[i + N + 1].split(""));
     }
-    for (let i = 0; i < N; i++) {
-        for (let j = 0; j < N; i++) {
-            if (T[0][0] == S[0][i])
+    for (let i = 1; i <= N; i++) {
+        for (let j = 1; j <= N; j++) {
+            if (j == 1 && trueCount != 0) {
+                index1++;
+                // console.log(`b = ${b}`);
+                index2 = 1;
+                j = b;
+            }
+            if (T[index1][index2] == S[i][j]) {
+                // console.log(i, j);
+                if (index1 == 1 && index2 == 1) {
+                    a = i;
+                    b = j;
+                }
+                index2++;
+                trueCount++;
+                if (trueCount == M ** 2) {
+                    isTrue = true;
+                    console.log(a + " " + b);
+                    break;
+                }
+                // console.log(trueCount);
+            } else {
+                if (trueCount != 0) {
+                    i = a;
+                    j = b;
+                }
+                trueCount = 0;
+                index1 = 1;
+                index2 = 1;
+            }
         }
+        if (trueCount == M ** 2) break;
+        // if (trueCount != 0) {
+        //     index1++;
+        //     console.log(`b = ${b}`);
+        //     index2 = b;
+        //     j = b;
+        // }
     }
-    console.log(N, M);
-    console.log(S);
-    console.log(T);
+
+    // console.log(S);
+    // console.log(T);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
