149357bc0ef47a8d49a069ea822c9a35ba550c9f
syu-ka <s3622021@campus.nias.ac.jp>
2025-02-02 16:31:31 +0900

    初AC（コンテスト後、自力）

--- abc391/b/main.js
+++ abc391/b/main.js
@@ -1,13 +1,9 @@
 function Main(input) {
     const [N, M] = input[0].split(" ").map(Number);
-    let S = [""];
-    let T = [""];
-    let index1 = 1;
-    let index2 = 1;
+    let S = [];
+    let T = [];
     let isTrue = false;
-    let isPrevTrue = false;
     let trueCount = 0;
-    let a, b;
 
     for (let i = 0; i < N; i++) {
         S.push(input[i + 1].split(""));
@@ -15,49 +11,29 @@ function Main(input) {
     for (let i = 0; i < M; i++) {
         T.push(input[i + N + 1].split(""));
     }
-    for (let i = 1; i <= N; i++) {
-        for (let j = 1; j <= N; j++) {
-            if (j == 1 && trueCount != 0) {
-                index1++;
-                // console.log(`b = ${b}`);
-                index2 = 1;
-                j = b;
-            }
-            if (T[index1][index2] == S[i][j]) {
-                // console.log(i, j);
-                if (index1 == 1 && index2 == 1) {
-                    a = i;
-                    b = j;
+
+    for (let a = 0; a < N; a++) {
+        for (let b = 0; b < N; b++) {
+            if (S[a][b] == T[0][0]) {
+                for (let i = 0; i < M; i++) {
+                    for (let j = 0; j < M; j++) {
+                        if (S[a + i][b + j] == T[i][j]) {
+                            trueCount++;
+                            if (trueCount == M ** 2) {
+                                isTrue = true;
+                                console.log(`${a + 1} ${b + 1}`);
+                            }
+                        } else {
+                            trueCount = 0;
+                            break;
+                        }
+                    }
+                    if (trueCount == 0 || isTrue) break;
                 }
-                index2++;
-                trueCount++;
-                if (trueCount == M ** 2) {
-                    isTrue = true;
-                    console.log(a + " " + b);
-                    break;
-                }
-                // console.log(trueCount);
-            } else {
-                if (trueCount != 0) {
-                    i = a;
-                    j = b;
-                }
-                trueCount = 0;
-                index1 = 1;
-                index2 = 1;
             }
         }
-        if (trueCount == M ** 2) break;
-        // if (trueCount != 0) {
-        //     index1++;
-        //     console.log(`b = ${b}`);
-        //     index2 = b;
-        //     j = b;
-        // }
     }
 
-    // console.log(S);
-    // console.log(T);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
