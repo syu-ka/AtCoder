94cbcba2c26b71aff45aadadecc9181dae81b5cd
syu-ka <s3622021@campus.nias.ac.jp>
2025-02-08 21:08:14 +0900

    初AC

--- abc392/a/main.js
+++ abc392/a/main.js
@@ -1,8 +1,12 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const A = input[0].split(" ").map(Number);
+    // console.log(A);
+
+    if (A[0] * A[1] == A[2] || A[0] * A[2] == A[1] || A[1] * A[2] == A[0]) {
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
