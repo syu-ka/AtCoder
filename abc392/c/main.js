156f882fecf0ba848c90854c49e27a9c3fdccf0b
syu-ka <s3622021@campus.nias.ac.jp>
2025-02-08 21:59:36 +0900

    初AC

--- abc392/c/main.js
+++ abc392/c/main.js
@@ -1,8 +1,17 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const N = Number(input[0]);
+    const P = input[1].split(" ").map(Number); //人iが人Piを見つめている.
+    const Q = input[2].split(" ").map(Number); //人iが数Qiのゼッケンを付けている.
+    let S = []; //iが書かれたゼッケンを付けている人が見つめている人のゼッケンの数.
+    for (let i = 0; i < N; i++) {
+        S.push(-1);
+    }
+
+    for (let i = 0; i < N; i++) {
+        S[Q[i] - 1] = Q[P[i] - 1];
+    }
+    console.log(...S);
+
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
