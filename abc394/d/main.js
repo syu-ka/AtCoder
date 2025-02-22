0c9d5f238f0cba3f23013f198366a01009ef5743
syu-ka <s3622021@campus.nias.ac.jp>
2025-02-22 23:03:13 +0900

    WA:17,AC26

--- abc394/d/main.js
+++ abc394/d/main.js
@@ -1,8 +1,88 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const s = input[0].split("");
+    let eCount = 0;
+    let eCount_continue = 0;
+    for (let i = 0; i < s.length; i++) {
+        // console.log(`i=${i}`);
+        // console.log(`eCount_continue=${eCount_continue}`);
+
+        switch (s[i]) {
+            case "(":
+                eCount_continue = 0;
+                break;
+
+            case ")":
+                // console.log(")");
+
+                if (s[(i - 1) - eCount_continue] == "(") {
+                    // console.log("(");
+                    eCount += 2;
+                    if ((i - 1) - eCount_continue <= 0) {
+                        eCount_continue = 0;
+                    } else {
+                        eCount_continue += 2;
+                    }
+                } else {
+
+                    eCount_continue = 0;
+
+                }
+                break;
+
+            case "[":
+                eCount_continue = 0;
+                break;
+
+            case "]":
+                // console.log("]");
+
+                if (s[(i - 1) - eCount_continue] == "[") {
+                    // console.log("[");
+                    eCount += 2;
+                    if ((i - 1) - eCount_continue <= 0) {
+                        eCount_continue = 0;
+                    } else {
+                        eCount_continue += 2;
+                    }
+                } else {
+
+                    eCount_continue = 0;
+
+                }
+                break;
+
+            case "<":
+                eCount_continue = 0;
+                break;
+
+            case ">":
+                // console.log(">");
+                if (s[(i - 1) - eCount_continue] == "<") {
+                    // console.log("<");
+                    eCount += 2;
+                    if ((i - 1) - eCount_continue <= 0) {
+                        eCount_continue = 0;
+                    } else {
+                        eCount_continue += 2;
+                    }
+                } else {
+
+                    eCount_continue = 0;
+
+                }
+                break;
+
+            default:
+                break;
+        }
+
+    }
+    // console.log(eCount);
+    if (eCount == s.length) {
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
