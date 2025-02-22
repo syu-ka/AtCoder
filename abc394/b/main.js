3568b78295e0e0f19a58d8c28b35166b10009274
syu-ka <s3622021@campus.nias.ac.jp>
2025-02-22 21:41:36 +0900

    初AC

--- abc394/b/main.js
+++ abc394/b/main.js
@@ -1,8 +1,29 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
-}
+    const n = Number(input[0]);
+    const s = [];
+    let sLength = [];
+    let answer = new Array(50);
+    for (let i = 0; i < n; i++) {
+        s.push(input[i + 1].split(""));
+        sLength.push(s[i].length);
+        // console.log(s[i]);
+        answer[sLength[i] - 1] = s[i].join("");
+    }
 
+    // let sSort = sLength.slice();
+    // sSort.sort(compareFn);
+
+    // for(let i = 0; i < n; i++){
+    //     if(sSort[i])
+    // }
+
+    // console.log(sSort);
+    // console.log(sLength);
+    // console.log(s[0]);
+    // console.log(answer);
+    console.log(answer.join(""));
+}
+function compareFn(a, b) {
+    return a - b;
+}
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
