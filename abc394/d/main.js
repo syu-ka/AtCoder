65a97950e9a199118f9f6a842e666692d09bc647
syu-ka <s3622021@campus.nias.ac.jp>
2025-02-22 23:30:51 +0900

    WA:10,AC:33(違う種類同士の括弧で閉じたらNo)

--- abc394/d/main.js
+++ abc394/d/main.js
@@ -1,75 +1,26 @@
 function Main(input) {
+    let answer = "Yes";
     const s = input[0].split("");
-    let eCount = 0;
-    let eCount_continue = 0;
+
+    //ペアで(2個ずつ)削除するため、奇数だと残ることが確定のため、"No".
+    if (s.length % 2 != 0) answer = "No";
+
+
     for (let i = 0; i < s.length; i++) {
-        // console.log(`i=${i}`);
-        // console.log(`eCount_continue=${eCount_continue}`);
 
         switch (s[i]) {
             case "(":
-                eCount_continue = 0;
-                break;
-
-            case ")":
-                // console.log(")");
-
-                if (s[(i - 1) - eCount_continue] == "(") {
-                    // console.log("(");
-                    eCount += 2;
-                    if ((i - 1) - eCount_continue <= 0) {
-                        eCount_continue = 0;
-                    } else {
-                        eCount_continue += 2;
-                    }
-                } else {
-
-                    eCount_continue = 0;
-
-                }
+                if (s[i + 1] == "]" || s[i + 1] == ">") answer = "No";
                 break;
 
             case "[":
-                eCount_continue = 0;
-                break;
+                if (s[i + 1] == ")" || s[i + 1] == ">") answer = "No";
 
-            case "]":
-                // console.log("]");
-
-                if (s[(i - 1) - eCount_continue] == "[") {
-                    // console.log("[");
-                    eCount += 2;
-                    if ((i - 1) - eCount_continue <= 0) {
-                        eCount_continue = 0;
-                    } else {
-                        eCount_continue += 2;
-                    }
-                } else {
-
-                    eCount_continue = 0;
-
-                }
                 break;
 
             case "<":
-                eCount_continue = 0;
-                break;
+                if (s[i + 1] == "]" || s[i + 1] == ")") answer = "No";
 
-            case ">":
-                // console.log(">");
-                if (s[(i - 1) - eCount_continue] == "<") {
-                    // console.log("<");
-                    eCount += 2;
-                    if ((i - 1) - eCount_continue <= 0) {
-                        eCount_continue = 0;
-                    } else {
-                        eCount_continue += 2;
-                    }
-                } else {
-
-                    eCount_continue = 0;
-
-                }
                 break;
 
             default:
@@ -77,12 +28,14 @@ function Main(input) {
         }
 
     }
-    // console.log(eCount);
-    if (eCount == s.length) {
+
+    if (answer == "Yes") {
         console.log("Yes");
     } else {
         console.log("No");
     }
+
+
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
