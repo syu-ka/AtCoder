609113fd144bb34ab2f35610b8deb58365f4a0b8
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-01 21:53:05 +0900

    初AC

--- abc395/b/main.js
+++ abc395/b/main.js
@@ -1,8 +1,38 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const n = Number(input[0]);
+    let s = [];
+
+    for (let i = 0; i < n; i++) {
+        s.push([...Array(n)]);
+    }
+
+    let j;
+    for (let i = 1; i <= n; i++) {
+        j = n + 1 - i;
+        if (i <= j) {
+            if (i % 2 == 1) {
+                let color = "#";
+                color_judge(color, s, i, j);
+            } else {
+                let color = ".";
+                color_judge(color, s, i, j);
+            }
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        console.log(s[i].join(""));
+    }
+}
+
+function color_judge(color, s, i, j) {
+    for (let a = i; a <= j; a++) {
+        // "-1"が美しくないので後で書き直す.
+        s[i - 1][a - 1] = color;
+        s[a - 1][i - 1] = color;
+        s[a - 1][j - 1] = color;
+        s[j - 1][a - 1] = color;
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
