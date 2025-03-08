ab2386e14287bf07911d5502c5b31c56e0442258
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-08 22:19:07 +0900

    WA:1,AC:51(一旦Dに行くため一時保存)

--- abc396/c/main.js
+++ abc396/c/main.js
@@ -1,8 +1,31 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [n, m] = input[0].split(" ").map(Number);
+    const b = input[1].split(" ").map(Number);
+    const w = input[2].split(" ").map(Number);
+
+    let valueSum = 0;
+    let ballCount = 0;
+    b.sort((a, b) => b - a);
+    w.sort((a, b) => b - a);
+    for (let i = 0; i < n; i++) {
+        if (b[i] + w[i] >= 0) {
+            valueSum += b[i];
+            ballCount++;
+        } else if (b[i] >= 0) {
+            valueSum += b[i];
+            ballCount++;
+        } else {
+            break;
+        }
+    }
+
+    for (let i = 0; i < ballCount; i++) {
+        if (w[i] < 0) {
+            break;
+        }
+        valueSum += w[i];
+    }
+    console.log(valueSum);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
