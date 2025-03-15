0253e7e9405c6641dfb179055c8bc3246d0fcd09
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-15 21:01:08 +0900

    new file

--- abc396/d/main.js
+++ abc396/d/main.js
@@ -1,5 +1,10 @@
 function Main(input) {
-    const test1 = Number(input[0]);
+    const [n, m] = Number(input[0]);
+    // n:頂点の数, m:辺の数.
+    for (let i = 1; i <= m; i++) {
+        let [u, v, w] = input[i].split(" ").map(Number);
+        console.log(a, b);
+    }
     const test2 = input[1].split(" ").map(Number);
     console.log(test1);
     console.log(test2);
