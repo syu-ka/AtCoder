38203900eb9ff2cbacdff3e7bf5658f71f6cfc8c
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-15 21:05:51 +0900

    初AC

--- abc397/a/main.js
+++ abc397/a/main.js
@@ -1,8 +1,14 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const x = Number(input[0]);
+    let answer;
+    if (x >= 38.0) {
+        answer = 1;
+    } else if (x >= 37.5) {
+        answer = 2;
+    } else {
+        answer = 3;
+    }
+    console.log(answer);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
