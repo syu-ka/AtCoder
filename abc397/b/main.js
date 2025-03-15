adf97b6249ef7bed5a9716aafeb816829c7c7c04
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-15 21:32:13 +0900

    WA 方向転換前

--- abc397/b/main.js
+++ abc397/b/main.js
@@ -1,8 +1,19 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const s = input[0].split("");
+    let count = 0;
+    const lengthTrue = s.length % 2 === 0 ? true : false;
+    // console.log(length);
+    for (let i = 0; i < s.length; i++) {
+        if (((i + count) % 2 === 1 && s[i] === "o") || ((i + count) % 2 === 0 && s[i] === "i")) {
+        } else {
+            count++;
+            // console.log(`i=${i}, count=${count}`);
+        }
+    }
+    if (!lengthTrue) {
+        count++;
+    }
+    console.log(count);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
