9b55e611e53b5e6dd11dc4c5b60ef4c45b135b91
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-15 22:26:25 +0900

    WA

--- abc397/c/main.js
+++ abc397/c/main.js
@@ -1,8 +1,35 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const n = Number(input[0]);
+    const a = input[1].split(" ").map(Number);
+
+    let count = {};
+    let split_index = n - 1;
+
+    for (let i = 0; i < n; i++) {
+        let elm = a[i];
+        let curt_index = i;
+
+        // 過去にelmが出てきたことがある.
+        if (count[elm]) {
+            if (count[elm] === 1) {
+                split_index = curt_index;
+            }
+
+            // 最新のcountを設定.
+            count[elm] = (count[elm] || 0) + 1;
+
+        } else {
+            //初めて出てきた数字elmのcountを設定.
+            count[elm] = (count[elm] || 0) + 1;
+        }
+    }
+
+    // console.log("split_index=" + split_index);
+    const firstHalfSet = new Set(a.slice(0, split_index));
+    // console.log(firstHalfSet);
+    const secondHalfSet = new Set(a.slice(split_index));
+    // console.log(secondHalfSet);
+    console.log(firstHalfSet.size + secondHalfSet.size);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
