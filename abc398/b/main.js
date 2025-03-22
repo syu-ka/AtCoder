da7240a1e06e8ecd31177197c002d10213631ac4
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-22 21:34:59 +0900

    初AC

--- abc398/b/main.js
+++ abc398/b/main.js
@@ -1,8 +1,28 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const a = input[0].split(" ").map(Number);
+    let count = {};
+
+    for (let i = 0; i < a.length; i++) {
+        let elm = a[i];
+        count[elm] = (count[elm] || 0) + 1;
+    }
+
+    let isX = false;    // 同じ数字のカードが三枚以上あるか.
+    let isY = false;    // 同じ数字のカードが二枚あるか.
+    for (elem in count) {
+        // console.log(`elem:${elem}, count[elem]:${count[elem]}`);
+        if (count[elem] >= 3 && !isX) {
+            isX = true;
+        } else if (count[elem] >= 2) {
+            isY = true;
+        }
+    }
+
+    if (isX && isY) {
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
