265de3d8fd31a6d8d9875d38bb4e3799280b4603
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-22 21:56:46 +0900

    初AC

--- abc398/c/main.js
+++ abc398/c/main.js
@@ -1,8 +1,28 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    let answer = -1;
+    let maxNUM = -1;
+
+    const n = Number(input[0]);
+    const a = input[1].split(" ").map(Number);
+
+    let count = {};
+    let index = {};
+
+    for (let i = 0; i < a.length; i++) {
+        let elm = a[i];
+        count[elm] = (count[elm] || 0) + 1;
+        index[elm] = i + 1;
+    }
+
+    for (elem in count) {
+        // console.log(`elem:${elem}, count[elem]:${count[elem]}`);
+        if (count[elem] === 1 && Number(elem) > maxNUM) {
+            maxNUM = Math.max(maxNUM, Number(elem));
+            answer = index[elem];
+        }
+    }
+
+    console.log(answer);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
