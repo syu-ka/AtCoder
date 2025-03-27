850920184c5a7876230e39695fdf750ad1fe1f7c
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-28 02:03:41 +0900

    初AC(解説視聴後+そのほかのJavaScriptのAC回答を見る)

--- abc385/c/main.js
+++ abc385/c/main.js
@@ -1,8 +1,26 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const n = Number(input[0]);
+    const h = input[1].split(" ").map(Number);
+
+    let answer = 1; // n=1の場合ループが実行されないため、初期値を1に設定.
+    for (let i = 1; i < n; i++) { // 公差i.
+        for (let j = 0; j < i; j++) { //初項j.
+            let height = 0;
+            let countCurrent = 0;
+            for (let k = j; k < n; k += i) { //初項j, 公差iの等差数列を作成.
+                /* 等差数列内の隣接する項で「ビルの高さ」が同じ場合、その隣接するビルは等間隔で並んでいると言える */
+                if (h[k] !== height) { // 高さが変わったら、初期化.
+                    countCurrent = 0;
+                    height = h[k];
+                }
+                countCurrent++;
+                answer = Math.max(answer, countCurrent);
+            }
+        }
+
+    }
+
+    console.log(answer);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
