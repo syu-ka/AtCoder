9a336b7e30493cc65a6d40aa966ecab357e6c981
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-27 23:18:27 +0900

    初AC

--- abc385/a/main.js
+++ abc385/a/main.js
@@ -1,8 +1,13 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const a = input[0].split(" ").map(Number);
+    let aSort = a.sort((a, b) => a - b);
+    // 全て等しい、又は、最大値とその他の和が等しいときYes.
+    if (aSort[0] === aSort[2] || aSort[0] + aSort[1] === aSort[2]) {
+        console.log("Yes");
+    }
+    else {
+        console.log("No");
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
