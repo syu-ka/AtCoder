4dd6a239f98cce1fb1b4b42a306f1ab64fe2e4ec
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-29 20:42:28 +0900

    入力を受け取る部分まで

--- ahc045/a/main.js
+++ ahc045/a/main.js
@@ -1,8 +1,26 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [n, m, q, l, w] = input[0].split(" ").map(Number);
+    // n: 都市の個数.
+    // m: 回答における都市のグループの個数.
+    // q: 最大クエリ回数.
+    // l: クエリを行う都市の集合のサイズの上限.
+    // w: 都市の座業が含まれる長方形の幅や高さとしてあり得る最大値.
+
+    const g = input[1].split(" ").map(Number);
+    // g[i]: i番目のグループに割り当てる都市の個数.
+
+    /* 各都市が存在する可能性のある座業の長方形範囲 */
+    let lx = [];
+    let rx = [];
+    let ly = [];
+    let ry = [];
+    for (let i = 0; i < n; i++) {
+        let tempXY = input[i + 2].split(" ").map(Number);
+        lx.push(tempXY[0]);
+        rx.push(tempXY[1]);
+        ly.push(tempXY[2]);
+        ry.push(tempXY[3]);
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
