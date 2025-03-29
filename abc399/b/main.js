5c2ab76b8007470284b42c9592b164663113bf7a
syu-ka <s3622021@campus.nias.ac.jp>
2025-03-29 21:45:52 +0900

    初AC

--- abc399/b/main.js
+++ abc399/b/main.js
@@ -1,8 +1,30 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const n = Number(input[0]);
+    const p = input[1].split(" ").map(Number);
+
+    const p_and_index = new Array(n);
+    for (let i = 0; i < n; i++) {
+        p_and_index[i] = { index: i, value: p[i] };
+    }
+
+
+    const p_and_index_sorted = [...p_and_index].sort((a, b) => b.value - a.value);
+    // p_sorted: pを逆順ソートした配列.
+
+    let rank = 1;
+    for (let i = 0; i < n; i++) {
+        if (p_and_index_sorted[i].value !== p_and_index_sorted[i - 1]?.value) {
+            rank = i + 1;
+        }
+        p_and_index_sorted[i].rank = rank;
+    }
+
+    // console.log(p_and_index);
+    // console.log(p_and_index_sorted);
+
+    for (let i = 0; i < n; i++) {
+        console.log(p_and_index[i].rank);
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
