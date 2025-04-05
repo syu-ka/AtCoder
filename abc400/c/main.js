ccd9c5a4a4de652c12b0b1bcaa810eeb5679c1b2
syu-ka <s3622021@campus.nias.ac.jp>
2025-04-05 22:14:35 +0900

    TLE14:AC15

--- abc400/c/main.js
+++ abc400/c/main.js
@@ -1,8 +1,22 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    let count = 0;
+    // let set = new Set();
+    const n = BigInt(input[0]);
+
+    for (let i = 2; i <= n; i *= 2) {
+        for (let j = 1; j * j * i <= n; j += 2) {
+            // if (set.has(j * j * i)) {
+            //     console.log(`j * j * i: ${j} * ${j} * ${i}= ${j * j * i}`);
+            // }
+            // console.log(`j * j * i: ${j} * ${j} * ${i}= ${j * j * i}`);
+            count++;
+            // set.add(j * j * i);
+        }
+    }
+
+    console.log(count);
+    // console.log(set);
+    // console.log(set.size);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
