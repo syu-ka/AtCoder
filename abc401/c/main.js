7f0ede48ba86511a4d50f4c64c4b0b2b4b8cf720
syu-ka <s3622021@campus.nias.ac.jp>
2025-04-13 00:38:15 +0900

    初AC（コンテスト終了後に初めて解けた）

--- abc401/c/main.js
+++ abc401/c/main.js
@@ -1,8 +1,18 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [n, k] = input[0].split(" ").map(Number);
+    let a = [...Array(n + 1).fill(1)];
+    const divisor = 1_000_000_000; // 割る数.
+
+    let s = k;
+    for (let i = k; i <= n; i++) {
+        a[i] = s;
+        s -= a[i - k];
+        s += a[i];
+        s = (s + divisor) % divisor;
+        // s = s % divisorではダメな理由：割られる数が負になるのを防ぐため.負になると割り算の結果が変わり、結果が正しく無くなる可能性がある.
+    }
+    console.log(a[n]);
+    // console.log(a);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
