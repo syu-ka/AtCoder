6e58799fc104ac8a9312b3947b95c4fdf2817c15
syu-ka <s3622021@campus.nias.ac.jp>
2025-04-19 21:33:10 +0900

    初AC

--- abc402/b/main.js
+++ abc402/b/main.js
@@ -1,8 +1,15 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const q = Number(input[0]);
+    let queue = [];
+    for (let i = 1; i <= q; i++) {
+        const query = input[i].split(" ").map(Number);
+        if (query[0] === 1) {
+            queue.push(query[1]);
+        } else if (query[0] === 2) {
+            console.log(queue[0]);
+            queue.shift();
+        }
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
