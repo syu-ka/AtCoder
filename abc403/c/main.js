6d84935202c7febbbe7d4431c04c0fde23202cf7
syu-ka <s3622021@campus.nias.ac.jp>
2025-04-27 22:54:34 +0900

    コンテスト終了後に開設無しで初AC

--- abc403/c/main.js
+++ abc403/c/main.js
@@ -1,8 +1,32 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [n, m, q] = input[0].split(" ").map(Number);
+    // n: 人数. m:ページ数. q: クエリ数.
+    let hasAllPage = Array(n).fill(false);
+    // hasAllPage[i]: i番目の人が全てのページの閲覧権限を持っているかどうか.
+    let hasEachPage = Array.from({ length: n }, () => new Set());
+    // hasEachPage[i]: i番目の人が持っているページのセット.
+
+    // console.log(hasAllPage);
+    // console.log(hasEachPage);
+
+    for (let i = 1; i <= q; i++) {
+        const [a, b, c] = input[i].split(" ").map(Number);
+        if (a === 1) {
+            hasEachPage[b - 1].add(c - 1);
+        } else if (a === 2) {
+            hasAllPage[b - 1] = true;
+        } else {
+            if (hasAllPage[b - 1]) {
+                console.log("Yes");
+            } else {
+                if (hasEachPage[b - 1].has(c - 1)) {
+                    console.log("Yes");
+                } else {
+                    console.log("No");
+                }
+            }
+        }
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
