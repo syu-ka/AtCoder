8535fe41223139e3cbd2350d82a53a6f1742a8fb
syu-ka <s3622021@campus.nias.ac.jp>
2025-05-03 21:39:20 +0900

    初AC

--- abc404/a/main.js
+++ abc404/a/main.js
@@ -1,8 +1,16 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const s = input[0].split("");
+    let allAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    // allAlphabet: 全てのアルファベットのセット.
+
+    const set = new Set(s);
+    for (let i = 0; i < allAlphabet.length; i++) {
+        if (!set.has(allAlphabet[i])) {
+            console.log(allAlphabet[i]);
+            return
+        }
+    }
+
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
