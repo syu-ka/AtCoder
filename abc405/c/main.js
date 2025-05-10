9d098fa106fdb4a0170df45202be6a7bdc9f6fa0
syu-ka <s3622021@campus.nias.ac.jp>
2025-05-10 22:09:21 +0900

    初AC

--- abc405/c/main.js
+++ abc405/c/main.js
@@ -1,8 +1,33 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const n = Number(input[0]);
+
+    // const a = input[1].split(" ").map(Number);
+    const a = input[1].split(" ").map(BigInt);
+
+    // let tmp_sum = 0;
+    let tmp_sum = BigInt(0);
+
+    // let sum = new Array(n).fill(0); // sum[i]:a[i]からa[n-1]までの和.
+    let sum = new Array(n).fill(BigInt(0)); // sum[i]:a[i]からa[n-1]までの和.
+
+    // let answer = 0;
+    let answer = BigInt(0);
+    // console.log(sum);
+
+    for (let i = 1; i < n; i++) {
+        tmp_sum += a[n - i];
+        sum[n - i] = tmp_sum;
+    }
+    // console.log(sum);
+
+    for (let i = 0; i < n - 1; i++) {
+        answer += a[i] * sum[i + 1];
+    }
+    // console.log(n);
+    // console.log(a);
+
+    // console.log(answer);
+    console.log(answer.toString());
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
