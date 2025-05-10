320665ba0355e6b49f7eea059b6058045c806c2d
syu-ka <s3622021@campus.nias.ac.jp>
2025-05-10 21:08:55 +0900

    初AC

--- abc405/a/main.js
+++ abc405/a/main.js
@@ -1,8 +1,21 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [r, x] = input[0].split(" ").map(Number);
+    const DIV1_MIN = 1600;
+    const DIV1_MAX = 2999;
+    const DIV2_MIN = 1200;
+    const DIV2_MAX = 2399;
+    if (x == 1) {
+        if (r >= DIV1_MIN && r <= DIV1_MAX) {
+            console.log("Yes");
+            return;
+        }
+    } else if (x == 2) {
+        if (r >= DIV2_MIN && r <= DIV2_MAX) {
+            console.log("Yes");
+            return;
+        }
+    }
+    console.log("No");
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
