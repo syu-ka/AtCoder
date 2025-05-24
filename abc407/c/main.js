fd7df15931065e895d544bea4b88f21710008393
syu-ka <s3622021@campus.nias.ac.jp>
2025-05-24 21:54:44 +0900

    初AC

--- abc407/c/main.js
+++ abc407/c/main.js
@@ -1,8 +1,11 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const s = input[0].split("").map(Number);
+    const n = s.length;
+    let difference_sum = 0;
+    for (let i = 0; i < n - 1; i++) {
+        difference_sum += s[i] - s[i + 1] >= 0 ? s[i] - s[i + 1] : 10 + s[i] - s[i + 1];
+    }
+    console.log(difference_sum + n + s[n - 1]);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
