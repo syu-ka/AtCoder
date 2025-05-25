d7b6925196c004655dd6cf032b9277f9efe8e832
syu-ka <s3622021@campus.nias.ac.jp>
2025-05-25 10:56:31 +0900

    解けずにコンテスト終了

--- abc407/d/main.js
+++ abc407/d/main.js
@@ -1,8 +1,40 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [h, w] = input[0].split(" ").map(Number);
+    const n = h * w;
+    let allC = calcAllC(n); // 全ての2進数の組み合わせを計算.
+    let a = [];
+    let binary_number = Array.from({ length: h }, () => Array(w).fill(0));
+    for (let i = 1; i <= h; i++) {
+        a.push(input[i].split(" ").map(Number));
+    }
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            binary_number[i][j] = a[i][j].toString(2);
+        }
+    }
+
+    // 全ての2進数の組み合わせを1つずつ処理.
+    for (target of allC) {
+        let targetC = target.split("").map(Number);
+        let sum = 0;
+
+        // 2進数の組み合わせを1桁ずつ処理.
+        for (let i = 0; i < targetC.length; i++) {
+
+        }
+    }
+    console.log(h, w);
+    console.log(a);
+    console.log(binary_number);
+}
+
+function calcAllC(n) {
+    newBox = [];
+    for (let i = 0; i < (1 << n); i++) {
+        newBox.push(i.toString(2).padStart(n, '0'));
+    };
+
+    return newBox;
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
