1553a82a49d98baa4d63b3b7033840e50c8267eb
syu-ka <s3622021@campus.nias.ac.jp>
2025-06-07 21:42:39 +0900

    初AC(iの範囲にnを含む事を見落とし時間ロス)

--- abc409/b/main.js
+++ abc409/b/main.js
@@ -1,8 +1,23 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    // 条件を満たす最大の非負整数を求める問題.
+    // 条件：A に、x 以上の要素が重複を含めて x 回以上現れる。
+    const n = Number(input[0]);
+    const a = input[1].split(" ").map(Number);
+    let ans = 0;
+
+    for (let i = 0; i <= n; i++) {  // i <= n の理由は、n 個の要素があるので、i は 0 から n までの範囲で考える必要があるため。
+        let count = 0;
+        // i 以上の要素が何回あるかを数える.
+        for (let j = 0; j < n; j++) {
+            if (a[j] >= i) {
+                count++;
+            }
+        }
+        if (count >= i) {
+            ans = i;
+        } //else break;
+    }
+    console.log(ans);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
