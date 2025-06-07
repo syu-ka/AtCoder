89e6067c95d0a97a9a64700b76c16fd5c7548191
syu-ka <s3622021@campus.nias.ac.jp>
2025-06-07 22:38:30 +0900

    2度目のAC（コメントの修正、適切な変数名へ修正、余計な文の削除）

--- abc409/c/main.js
+++ abc409/c/main.js
@@ -3,28 +3,23 @@ function Main(input) {
     // 条件：3点の距離が等しい。
     const [n, l] = input[0].split(" ").map(Number); // n は点の数、l は円の周の長さ。
     const d = input[1].split(" ").map(Number);  // d は一つ前の点からの距離。
-    let ans = 0;
-    const ET_d = l / 3; // 正三角形の辺の長さを求めるために、l を 3 で割る.
+    let ans = 0;    // 組み合わせの数を格納する変数。
+    const ET_d = l / 3; // 正三角形の弧の長さを求めるために、l を 3 で割る.
 
     let count = {};
     count[0] = 1; // 初期値として0を設定.
-    for (let i = 0; i < l; i++) {
-        if (count[i] === undefined) {
-            count[i] = 0; // 存在しない場合は0を設定.
-        }
-    }
     let remainder = 0;
     for (let i = 0; i < n - 1; i++) {
-        let elem = (remainder + d[i]) % l;
-        count[elem] = (count[elem] || 0) + 1;
-        remainder = elem;
+        let pos = (remainder + d[i]) % l;
+        count[pos] = (count[pos] || 0) + 1;
+        remainder = pos;
     }
 
     for (let i = 0; i < l; i++) {
         let pos1 = count[i] || 0;
         let pos2 = count[i + ET_d] || 0;
         let pos3 = count[i + ET_d + ET_d] || 0;
-        // console.log(`i: ${i}, pos1: ${pos1}, pos2: ${pos2}, pos3: ${pos3}`);
+
         if (pos1 > 0 && pos2 > 0 && pos3 > 0) {
             // pos1, pos2, pos3 はそれぞれの点の数を表す.
             ans += pos1 * pos2 * pos3; // 組み合わせの数を計算.
@@ -35,7 +30,6 @@ function Main(input) {
         }
     }
 
-    // console.log(count);
     console.log(ans);
 }
 
