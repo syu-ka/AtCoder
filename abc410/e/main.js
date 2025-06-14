6b114b66247cf19a002626f15a6e5f950aa20563
syu-ka <s3622021@campus.nias.ac.jp>
2025-06-14 22:49:57 +0900

    コンテスト終了時

--- abc410/e/main.js
+++ abc410/e/main.js
@@ -1,8 +1,17 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [n, h, m] = input[0].split(" ").map(Number);
+    // n: モンスターの数, h: 高橋君の体力, m: 高橋君の魔力
+    let a = []; // a[i]:i番目のモンスターを高橋君の体力をa[i]削ることで倒すことができる
+    let b = []; // b[i]:i番目のモンスターを高橋君の魔力をb[i]削ることで倒すことができる
+    for (let i = 1; i <= n; i++) {
+        a.push(input[i].split(" ").map(Number)[0]);
+        b.push(input[i].split(" ").map(Number)[1]);
+    }
+    const a_and_index = Array.from({ length: n }, (_, i) => ({ value: a[i], index: i }));
+    const b_and_index = Array.from({ length: n }, (_, i) => ({ value: b[i], index: i }));
+    let a_sorted = a_and_index.sort((x, y) => x.value - y.value);
+    let b_sorted = b_and_index.sort((x, y) => x.value - y.value);
+    console.log(a_sorted, b_sorted);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
