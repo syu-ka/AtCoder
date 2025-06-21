a74d7d1cc6ebdd629d15df24c0e074ee0afc5967
syu-ka <s3622021@campus.nias.ac.jp>
2025-06-21 22:41:05 +0900

    初AC(コンテスト終了時点)

--- abc411/c/main.js
+++ abc411/c/main.js
@@ -1,8 +1,48 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [n, q] = input[0].split(" ").map(Number);
+    // n: マスの数. q: クエリの数.
+    const a = input[1].split(" ").map(Number);
+    // a[i]: i番目のマスを反転させる.
+
+    let tmp_array = Array.from({ length: n }, (_, i) => 0); //マスの状態.0:白, 1:黒.
+    let tmp_count = 0; // 黒く塗られたマスが連続している区間の個数.
+    for (let i = 0; i < a.length; i++) {
+        if (tmp_array[a[i] - 1] === 0) {
+            tmp_array[a[i] - 1] = 1;    // 黒にする.
+
+            if (n === 1) {
+                console.log(1);
+                continue;
+            }
+
+            if (a[i] === 1) { //a[i]が先頭の場合.
+                if (tmp_array[a[i]] === 0) tmp_count++;
+            } else if (a[i] === n) {   //a[i]が最後の場合.
+                if (tmp_array[a[i] - 2] === 0) tmp_count++;
+            } else {
+                if (tmp_array[a[i] - 2] === 1 && tmp_array[a[i]] === 1) tmp_count--;
+                if (tmp_array[a[i] - 2] === 0 && tmp_array[a[i]] === 0) tmp_count++;
+            }
+        } else {
+            tmp_array[a[i] - 1] = 0;    //白にする.
+
+            if (n === 1) {
+                console.log(0);
+                continue;
+            }
+
+            if (a[i] === 1) { //a[i]が先頭の場合.
+                if (tmp_array[a[i]] === 0) tmp_count--;
+            } else if (a[i] === n) {   //a[i]が最後の場合.
+                if (tmp_array[a[i] - 2] === 0) tmp_count--;
+            } else {
+                if (tmp_array[a[i] - 2] === 1 && tmp_array[a[i]] === 1) tmp_count++;
+                if (tmp_array[a[i] - 2] === 0 && tmp_array[a[i]] === 0) tmp_count--;
+            }
+        }
+        console.log(tmp_count);
+    }
+
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
