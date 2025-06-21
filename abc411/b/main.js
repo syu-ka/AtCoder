a4fc46c82ea719ae730a94f9f770503ecfb2ff97
syu-ka <s3622021@campus.nias.ac.jp>
2025-06-21 21:26:11 +0900

    初AC

--- abc411/b/main.js
+++ abc411/b/main.js
@@ -1,8 +1,15 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const n = Number(input[0]);
+    const d = input[1].split(" ").map(Number);
+    for (let i = 0; i < n - 1; i++) {
+        let output = [];
+        let tmp_sum = 0;
+        for (let j = i; j < n - 1; j++) {
+            tmp_sum += d[j];
+            output.push(tmp_sum);
+        }
+        console.log(output.join(" "));
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
