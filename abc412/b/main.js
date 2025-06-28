cd8c9ebbeea15045e29ddae76fe028abc205482f
syu-ka <s3622021@campus.nias.ac.jp>
2025-06-28 21:30:16 +0900

    初AC

--- abc412/b/main.js
+++ abc412/b/main.js
@@ -1,8 +1,21 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const s = input[0].split("");
+    const s_toLower = s.map(c => c.toLowerCase());
+    // s_toLower: sの小文字に変換したもの.
+    const t = input[1].split("");
+    const t_set = new Set(t); // tの文字をセットにする.重複を排除する.
+    // console.log(s, t, s_toLower);
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] !== s_toLower[i]) {
+            let prev = s[i - 1];
+            if (!t_set.has(prev)) {
+                console.log("No");
+                return;
+            }
+        }
+    }
+    console.log("Yes");
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
