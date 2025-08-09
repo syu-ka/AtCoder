2c841106de9d36513e9d0227cf1aee2c0fb14a42
syu-ka <s3622021@campus.nias.ac.jp>
2025-08-10 00:56:55 +0900

    初AC

--- abc418/c/main.js
+++ abc418/c/main.js
@@ -3,46 +3,48 @@ function Main(input) {
     const a = input[1].split(" ").map(Number);
     a.sort((x, y) => x - y);
     const a_max = a[a.length - 1];
-    console.log(n, q, a, a_max);
-    let count = {};
-    count[1] = 1; // 初期値として1を設定
+    // console.log(n, q, a, a_max);
+    let level_to_minX = {};
+    level_to_minX[1] = 1; // 初期値として1を設定
     let tmp_count = 0;
-    for (let i = 1; i <= a_max; i++) {
-        // if ()
-    }
+    let teaBag_count_to_teaKind_count = {}; // 茶葉の個数から茶葉の種類の個数へのマッピング
+
     for (let i = 0; i < n; i++) {
-        tmp_count += a[i];
-        count[a[i] + 1] = tmp_count + a[i];
-        // if (a[i] !== a[i - 1] || i === 0) {
-        //     tmp_continuous_count = 1;
-        //     count[a[i]] = tmp_count;
-        // } else {
-        //     tmp_continuous_count++;
-        //     count[a[i]] = tmp_count - tmp_continuous_count;
-        // }
+        teaBag_count_to_teaKind_count[a[i]] = (teaBag_count_to_teaKind_count[a[i]] || 0) + 1;
     }
-    console.log(count);
-    for (let i = a_max; i >= 0; i--) {
-        if (count[i] === undefined) {
-            count[i] = count[i + 1] - 1;
+    // console.log(`teaBag_count_to_teaKind_count=${JSON.stringify(teaBag_count_to_teaKind_count)}`);
+
+    let tmp_number_or_higher_count = a.length; // 現在の数以上の数の個数
+    for (let i = 2; i <= a_max; i++) {
+        // console.log(`---i=${i}---`);
+        tmp_number_or_higher_count -= teaBag_count_to_teaKind_count[i - 1] || 0;
+        if (teaBag_count_to_teaKind_count[i - 1] === undefined) {
+            // console.log("あ");
+            level_to_minX[i] = level_to_minX[i - 1] + tmp_number_or_higher_count;
+        } else {
+            level_to_minX[i] = level_to_minX[i - 1] + tmp_number_or_higher_count + teaBag_count_to_teaKind_count[i - 1];
         }
+        // console.log(`tmp_number_or_higher_count=${tmp_number_or_higher_count}`);
+        // console.log(`level_to_minX[${i}]=${level_to_minX[i]}`);
     }
-    console.log(`count[1]=${count[1]}`);
-    console.log(`count[2]=${count[2]}`);
-    console.log(`count[3]=${count[3]}`);
-    console.log(`count[4]=${count[4]}`);
-    console.log(`count[5]=${count[5]}`);
-    console.log(`count[6]=${count[6]}`);
-    console.log(`count[7]=${count[7]}`);
-    console.log(`count[8]=${count[8]}`);
-    console.log(`count[9]=${count[9]}`);
-    console.log(`count[10]=${count[10]}`);
-    console.log("---answer---");
+
+
+    // console.log(`level_to_minX[1]=${level_to_minX[1]}`);
+    // console.log(`level_to_minX[2]=${level_to_minX[2]}`);
+    // console.log(`level_to_minX[3]=${level_to_minX[3]}`);
+    // console.log(`level_to_minX[4]=${level_to_minX[4]}`);
+    // console.log(`level_to_minX[5]=${level_to_minX[5]}`);
+    // console.log(`level_to_minX[6]=${level_to_minX[6]}`);
+    // console.log(`level_to_minX[7]=${level_to_minX[7]}`);
+    // console.log(`level_to_minX[8]=${level_to_minX[8]}`);
+    // console.log(`level_to_minX[9]=${level_to_minX[9]}`);
+    // console.log(`level_to_minX[10]=${level_to_minX[10]}`);
+    // console.log("---answer---");
     for (let i = 0; i < q; i++) {
         const b = Number(input[i + 2]);
-        console.log(`b=${b}`);
+        // console.log(`b=${b}`);
         if (b <= a_max) {
-            console.log(count[b]);
+            console.log(level_to_minX[b]);
         } else {
             console.log(-1);
         }
