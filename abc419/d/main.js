715389e03b4f6d8e959f3dbbcd979b3e4056eb82
syu-ka <s3622021@campus.nias.ac.jp>
2025-08-16 23:29:59 +0900

    コンテスト終了時（A,B,C=AC、D=途中）

--- abc419/d/main.js
+++ abc419/d/main.js
@@ -2,21 +2,27 @@ function Main(input) {
     const [n, m] = input[0].split(" ").map(Number);
     const s = input[1].split("");
     const t = input[2].split("");
-    // let index_to_appearanceTime = {}; // インデックスと出現回数のマッピング
+    let index_to_appearanceTime = {}; // インデックスと出現回数のマッピング
 
     for (let i = 0; i < m; i++) {
         const [l, r] = input[3 + i].split(" ").map(Number);
         // console.log("-----");
         // console.log(`l=${l}, r=${r}`);
 
-        let subStr_s = s.slice(l - 1, r);
-        let subStr_t = t.slice(l - 1, r);
-        // console.log(subStr_s, subStr_t);
-        s.splice(l - 1, subStr_t.length, ...subStr_t);
-        t.splice(l - 1, subStr_s.length, ...subStr_s);
-        // console.log(s.join(""), t.join(""));
-
+        for (let j = l - 1; j < r; j++) {
+            index_to_appearanceTime[j] = (index_to_appearanceTime[j] || 0) + 1;
+            // console.log(`index_to_appearanceTime[${j}] = ${index_to_appearanceTime[j]}`);
+        }
     }
+
+    for (let i = 0; i < n; i++) {
+        if (index_to_appearanceTime[i] % 2 === 1) {
+            let tmpStr = s[i];
+            s[i] = t[i];
+            t[i] = tmpStr;
+        }
+    }
+
     console.log(s.join(""));
 }
 
