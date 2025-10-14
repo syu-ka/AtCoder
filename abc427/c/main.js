aa2d7bbc20afaa7f0052674bbc418e14e75fea5b
syu-ka <s3622021@campus.nias.ac.jp>
2025-10-14 10:59:18 +0900

    解説見た後（解説コードのpythonをJavaScriptに直し）AC

--- abc427/c/main.js
+++ abc427/c/main.js
@@ -1,63 +1,39 @@
 function Main(input) {
-    const [n, m] = input[0].split(" ").map(Number);
-    let u_v_graphArray = Array.from({ length: n }, (v, i) => []); // グラフの隣接リスト表現
-    for (let i = 1; i <= m; i++) {
-        u_v_graphArray[i - 1] = input[i].split(" ").map(Number);
+    /* N 頂点 M 辺の単純な無向グラフ.
+     グラフを二部グラフにするために、最低でも何回辺を削除する必要があるか. */
+
+    /* 回答方針
+        適当に頂点を塗り分ける.
+        同じ色の頂点を結ぶ辺を削除すると、二部グラフになる.
+        そのため、「同じ色の頂点を結ぶ辺の数」が「削除する辺の数」になる.
+        頂点の塗り分け方は 2^N 通りあるので、bit全探索してすべての塗り方での「削除する辺の数」を求め、その最小値が答えになる.
+    */
+
+    const [N, M] = input[0].split(" ").map(Number);
+    let edges = []; // グラフの隣接リスト表現
+    for (let i = 1; i <= M; i++) {
+        const [u, v] = input[i].split(" ").map(Number);
+        edges.push([u, v]);
     }
-    // console.log(u_v_graphArray);
 
+    let ans = M;
+    /*  2^N 通りの塗り方を全部探索する */
+    for (let bit = 0; bit < (1 << N); bit++) {
+        // console.log(`bit=`, bit);
+        console.log(`bit=`, bit.toString(2).padStart(N, '0'));
+        let delete_count = 0
+        for (const [u, v] of edges) {  // それぞれの辺を見て
+            // console.log(`u=`, u, `v=`, v);
+            // console.log(`(bit >> u)=`, (bit >> u), `(bit >> v)=`, (bit >> v));
 
-    const checkBipartite = (number, graphArray) => {
-        let i = 0;
-        const colors = new Array(number).fill(0);
-
-        // 深さ優先探索 他の頂点とを網羅するために使用する。
-
-        const dfs = (v, c) => {
-            // console.log(`${v}の色を${c}色に塗る`);
-            colors[v] = c;
-            // console.log(`${i}|なし|def(${i},${c}) | |${v}を${c}色に塗る|${colors}`);
-            for (let j = 0; j < graphArray[v].length; j++) {
-                // 頂点vに対して、隣接する頂点毎にループする。
-                // 隣接している頂点が同じ色の場合
-                if (colors[graphArray[v][j]] == c) {
-                    // console.log(`${i}|${j}|def(${i},${c}) | colors[graphArray[v][j]] == c  </br> colors[graphArray[${v}][${j}]]（ ＝${colors[graphArray[v][j]]}） == ${c}|${v}と${graphArray[v][j]}は同じ${c}色${colors}`);
-                    return false;
-                }
-                // 隣接している頂点が塗られていない場合異なる色で塗る。
-                if (colors[graphArray[v][j]] == 0 && !dfs(graphArray[v][j], -c)) {
-                    // console.log(`${i}|${j}| def(${graphArray[v][j]},${-c} | colors[graphArray[v][j]] == 0  </br> colors[graphArray[${v}][${j}]]（ ＝${colors[graphArray[v][j]]}） == 0 |${v}は塗り替えられていないので${-c}色に塗る|${colors}`);
-                    return false;
-                }
-                // console.log(`${i}|${j}|def(${i},${c})| colors[graphArray[v][j]] !== 0 and c  </br> colors[graphArray[${v}][${j}]]（ ＝${colors[graphArray[v][j]]}） !== 0  and ${c}|${v}と${graphArray[v][j]}は既に異なる色に塗られている|${colors}`);
-            }
-            // 全ての色が塗れた場合。
-            // console.log("色が塗れた場合");
-            // console.log(`${i}|なし|def(${i},${c}) | |すべての頂点が塗れた|${colors}`);
-            return true;
-        }
-        // 全ての頂点に対してループする。
-        // console.log(`i|j|!dfs(i,1)|条件|行動|graphの色`)
-        for (i = 0; i < number; i++) {
-            if (colors[i] == 0) {
-                if (!dfs(i, 1)) {
-                    console.log("false");
-                    return "false";
-                }
+            /* 結んでいる頂点が同じ色で塗られていたら */
+            if ((1 & (bit >> u)) == (1 & (bit >> v))) {
+                delete_count += 1; // カウントを増やす
             }
         }
-        console.log("true");
-        return "true";
+        ans = Math.min(ans, delete_count)
     }
-
-    let is2colorable = false;
-    let i = 0;
-    while (is2colorable === false) {
-        u_v_graphArray.splice()
-        is2colorable = checkBipartite(n, u_v_graphArray);
-    }
-
-
+    console.log(ans);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
