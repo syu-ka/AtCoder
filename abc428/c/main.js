b1921438f9b70dcc9232b1c855200fe4c76d8786
syu-ka <s3622021@campus.nias.ac.jp>
2025-10-19 01:39:26 +0900

    解説読了後(C:AC)

--- abc428/c/main.js
+++ abc428/c/main.js
@@ -2,40 +2,37 @@ function Main(input) {
     const q = Number(input[0]);
     let type = [];
     let b = [];
-    let stack = [];
-    let shift_continue_count = 0;
+    let stack = [0];
+    let stack_prefixSum = [0];
+    let isNice = false;
     for (let i = 1; i <= q; i++) {
         type.push(input[i].split(" ")[0]);
         if (input[i].split(" ")[1]) {
             b.push(input[i].split(" ")[1]);
         }
 
+        let current_length = stack.length;
+
         // console.log(`----i=${i}----`);
 
         if (type[type.length - 1] == "1") {
+            stack.push(stack[current_length - 1]);
             if (b[b.length - 1] === "(") {
-                stack.unshift("(");
+                stack[current_length]++;
+                isNice = false;
             } else if (b[b.length - 1] === ")") {
-                if (stack.length === 0) {
-                    shift_continue_count = 0;
-                } else {
-                    shift_continue_count++;
-                    stack.shift();
-                }
+                stack[current_length]--;
             }
+            stack_prefixSum.push(Math.min(stack_prefixSum[current_length - 1], stack[current_length]));
         } else {
             let b_pop = b.pop();
-            if (b_pop === "(") {
-                stack.pop();
-            } else if (b_pop === ")") {
-                if (shift_continue_count > 0) {
-                    stack.unshift("(");
-                    shift_continue_count--;
-                }
-            }
+            stack.pop();
+            stack_prefixSum.pop();
         }
-        // console.log(b);
-        if (stack.length == 0) {
+
+        // console.log(`stack=${stack}`);
+        // console.log(`stack_prefixSum=${stack_prefixSum}`)
+        if (stack[stack.length - 1] == 0 && stack_prefixSum[stack_prefixSum.length - 1] == 0) {
             console.log("Yes");
         } else {
             console.log("No");
