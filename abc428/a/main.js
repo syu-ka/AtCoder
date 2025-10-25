9af78935230ad555dd6918df2524f354f7de47b2
syu-ka <s3622021@campus.nias.ac.jp>
2025-10-25 21:00:24 +0900

    new file

--- abc428/a/main.js
+++ abc428/a/main.js
@@ -1,8 +1,18 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [s, a, b, x] = input[0].split(" ").map(Number);
+    let tmp_time = 0;
+    let set_count = Math.floor(x / (a + b));    // a + bをセットと考え、x秒までに何セットあるのかの変数.
+
+    let set_remainder = x % (a + b); // set_countセットを終えてx秒までに何秒あるかの変数.
+
+    let set_remainder_a_time = a <= set_remainder ? a : set_remainder; // set_remainderの内、動いている(a)の秒数
+
+    let total_move_distance = ((a * s) * set_count) + set_remainder_a_time * s;
+
+    // console.log(set_count);
+    // console.log(set_remainder);
+    // console.log(set_remainder_a_time);
+    console.log(total_move_distance);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
--- abc428/b/main.js
+++ abc428/b/main.js
@@ -1,8 +1,24 @@
 function Main(input) {
-    const test1 = Number(input[0]);
-    const test2 = input[1].split(" ").map(Number);
-    console.log(test1);
-    console.log(test2);
+    const [n, k] = input[0].split(" ").map(Number);
+    const s = input[1].split("");
+
+    let word_to_appearanceCount = {};
+    for (let i = 0; i < n - (k - 1); i++) {
+        let tmp_word = "";
+        for (let j = i; j < i + k; j++) {
+            tmp_word += s[j];
+        }
+        // console.log(tmp_word);
+        word_to_appearanceCount[tmp_word] = (word_to_appearanceCount[tmp_word] || 0) + 1;
+    }
+    const max_appearanceCount = Math.max(...Object.values(word_to_appearanceCount));
+    console.log(max_appearanceCount);
+
+    const max_appearanceCount_word = Object.keys(word_to_appearanceCount).filter((key) => {
+        return word_to_appearanceCount[key] === max_appearanceCount
+    });
+    max_appearanceCount_word.sort();
+    console.log(max_appearanceCount_word.join(" "));
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
