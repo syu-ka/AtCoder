8e0b7d9a593ac90668385bb81df6e84892de1d28
syu-ka <s3622021@campus.nias.ac.jp>
2025-11-01 21:03:28 +0900

    new file

--- abc429/d/main.js
+++ abc429/d/main.js
@@ -11,25 +11,48 @@ function Main(input) {
         pos_to_count[a[i]] = (pos_to_count[a[i]] || 0) + 1;
     }
 
+    let pos_to_count_arr = []; // pos_to_countのkeyを格納する配列.
+    for (pos in pos_to_count) {
+        if (pos == 0) {
+            continue;
+        }
+        pos_to_count_arr.push(pos_to_count[pos]);
+    }
+    if (pos_to_count[0]) {
+        pos_to_count_arr.push(pos_to_count[0]);
+    }
+    // console.log(`pos_to_count_arr=${pos_to_count_arr}`);
+
     let meet_count = 0;
     let sum_meet_count = 0;
-    let left = 1;
-    let right = 1;
-    console.log(pos_to_count[m]);
-    for (let left = 1; left < m; left++) {
+    let left = 0;
+    let right = 0;
+    for (left = 0; left < pos_to_count_arr.length; left++) {
+        // console.log(`---left=${left}---`);
+        // if (left == 0) {
         for (let j = right; meet_count < c; j++) {
-            meet_count = meet_count + (pos_to_count[j] || 0);
-            right = j;
-            console.log(j);
+            let index = j % pos_to_count_arr.length;
+            meet_count += pos_to_count_arr[index];
+            right = index + 1;
+            // console.log(index);
+
         }
-        console.log(`---left=${left}---`);
-        console.log(`meet_count=${meet_count}`);
         sum_meet_count += meet_count;
+        // console.log(`meet_count=${meet_count}`);
+        for (let j = 0; meet_count >= c; j++) {
+            meet_count -= pos_to_count_arr[left];
+            left += j;
+        }
+        // console.log(`meet_count=${meet_count}`);
+
+
+        // console.log(`sum_meet_count=${sum_meet_count}`);
     }
 
-
-    console.log(pos_to_count);
-    console.log(sum_meet_count);
+    let result = sum_meet_count * (1 + m - pos_to_count_arr.length);
+    // console.log(pos_to_count);
+    // console.log(sum_meet_count);
+    console.log(result);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
