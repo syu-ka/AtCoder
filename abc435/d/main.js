92dc2ed22ac2aa88e34eeb6bf6e2a3dfe872ea98
syu-ka <s3622021@campus.nias.ac.jp>
2025-12-07 01:01:28 +0900

    テストケースでは通ったけど、本番ケースでは AC× 2, TLE× 11, MLE× 2, RE× 15

--- abc435/d/main.js
+++ abc435/d/main.js
@@ -3,54 +3,43 @@ function Main(input) {
     let point_to_reachable = {};
     for (let i = 1; i <= M; i++) {
         let [X, Y] = input[i].split(" ").map(Number);
-        point_to_reachable[X] = (point_to_reachable[X] || new Set());
-        point_to_reachable[X].add(Y);
+        point_to_reachable[X] = (point_to_reachable[X] || []);
+        point_to_reachable[X].push(Y);
     }
-    console.log(point_to_reachable);
-    // console.log(point_to_reachable[1]);
-    // console.log(point_to_reachable[2]);
-    // console.log(point_to_reachable[1].union(point_to_reachable[2]));
-    // let root_points = [...Array(point_to_reachable)];
-    // console.log(`root_points=${root_points}`);
-    let root_points = [];
-    for (point in point_to_reachable) {
-        root_points.push(point);
-    }
-    console.log("root_points=", root_points);
-    for (root_point of root_points) {
-        // console.log(root_point);
-        // console.log("point_to_reachable[root_point]", point_to_reachable[root_point]);
-        let points = [];
-        point_to_reachable[root_point].forEach((point) => {
-            points.push(point);
-            // console.log(`point = ${point} `);
-            // console.log("point_to_reachable[point]", point_to_reachable[point]);
-        })
-        for (point of points) {
-            point_to_reachable[root_point].add(point_to_reachable[point])
+    // console.log(point_to_reachable);
+
+    for (root_point in point_to_reachable) {
+        for (point of point_to_reachable[root_point]) {
+            point_to_reachable[root_point] = point_to_reachable[point] ? point_to_reachable[root_point].concat(point_to_reachable[point]) : point_to_reachable[root_point];
         }
     }
-
-    // for (root_point in point_to_reachable) {
-    //     console.log(`root_point=${root_point}`);
-    //     // let reachable_points = Array.from(point_to_reachable[root_point]);
-    //     // point_to_reachable[root_point].add(reachable_points);
-    //     // console.log(reachable_points);
-    //     console.log(`point_to_reachable[root_point]=${[...point_to_reachable[root_point]]} `);
-
-    //     // point_to_reachable[root_point].forEach((point) => {
-    //     //     console.log(`point = ${ point } `);
-    //     //     point_to_reachable[root_point].union(point_to_reachable[point]);
-    //     //     console.log(`point_to_reachable[root_point] = ${ point_to_reachable[root_point] } `);
-    //     //     console.log(point_to_reachable[point]);
-    //     // })
-    // }
-
-    const Q = input[M + 1].split(" ").map(Number);
-    for (let i = M + 2; i < (M + 2) + Q; i++) {
-
+    for (root_point in point_to_reachable) {
+        point_to_reachable[root_point] = new Set(point_to_reachable[root_point]);
     }
-    console.log(point_to_reachable);
+    // console.log(point_to_reachable);
+
+    const Q = Number(input[M + 1]);
+    let black_points = [];
+    for (let j = M + 2; j < (M + 2) + Q; j++) {
+        let [type, v] = input[j].split(" ").map(Number);
+        if (type == 1) {
+            black_points.push(v);
+        } else if (type == 2) {
+            let can_reachable = false;
+            for (bp of black_points) {
+                if (point_to_reachable[v].has(bp)) {
+                    console.log("Yes");
+                    can_reachable = true;
+                    break;
+                }
+            }
+            if (!can_reachable) {
+                console.log("No");
+            }
+        } else {
+            console.log("存在しません");
+        }
+    }
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
