87330d6a8d93d5644d01539c537a56dd0b8a527d
syu-ka <s3622021@campus.nias.ac.jp>
2026-01-03 23:04:35 +0900

    A:AC, B:AC, C:AC(コンテスト終了後15分）

--- abc439/c/main.js
+++ abc439/c/main.js
@@ -3,37 +3,39 @@ const { NONAME } = require("dns");
 function Main(input) {
     const N = Number(input[0]);
     let square_num = [];
-    let answer = [];
     for (let i = 1; i < N; i++) {
         let tmp_square_num = i ** 2;
         if (tmp_square_num >= N) break;
         square_num.push(tmp_square_num);
     }
 
+    let s_s_set = new Set(); // 平方数の和のSet.
+    let s_s_duplication = []; // 平方数の和で重複した値.
     for (let i = 0; i < square_num.length - 1; i++) {
         for (let j = i + 1; j < square_num.length; j++) {
             let tmp_answer = square_num[i] + square_num[j];
             if (tmp_answer > N) break;
-            answer.push(tmp_answer);
+            if (s_s_set.has(tmp_answer)) {
+                s_s_duplication.push(tmp_answer);
+            }
+            s_s_set.add(tmp_answer);
         }
+    }
+
+    // 重複した値を削除.
+    for (duplication of s_s_duplication) {
+        s_s_set.delete(duplication);
+    }
+
+    let answer = [];
+    for (ans of s_s_set) {
+        answer.push(ans);
     }
 
     answer.sort((a, b) => { return a - b; })
 
-
-    // console.log(square_num);
-    // console.log(answer.length);
-    let only_answer_arr = [];
-    let prev_only_answer = answer[0];
-    for (let i = 0; i < answer.length; i++) {
-        let tmp_only_answer = answer[i];
-        if (tmp_only_answer != prev_only_answer) {
-            only_answer_arr.push(only_ans);
-        }
-        prev_only_answer = tmp_only_answer;
-    }
-    console.log(only_answer_arr.length);
-    console.log(only_answer_arr.join(" "));
+    console.log(answer.length);
+    console.log(answer.join(" "));
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
