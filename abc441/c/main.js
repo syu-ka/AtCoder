cb017ab60d7d24fe403bcf39b25e8dcd088f6e6f
syu-ka <s3622021@campus.nias.ac.jp>
2026-01-17 23:03:14 +0900

    解説をみて、降順で良いことに気づき、昇順から降順に変えるとAC

--- abc441/c/main.js
+++ abc441/c/main.js
@@ -8,25 +8,21 @@ function Main(input) {
 
     const A = input[line++].split(" ").map(Number); // A[i]:i番目のカップに入っている液体の量(mL).
     const A_sort = A.toSorted((a, b) => {
-        return a - b;
-    });
+        return b - a;
+    }); // 降順. // 液体の量は分かっているため、より多くお酒の飲める確率の高い、液体の量が多いカップから飲むのが最適なため、降順に並べて置く.
 
     let current_mL = 0;
-    let ans_cap = Infinity;
+    let ans_cap = -1;
 
     // お酒のカップを調べる.
     for (let i = 1; i <= K; i++) {
-        current_mL += A_sort[i - 1]; //i-1なのはA_sortが0-indexedであるため
+        current_mL += A_sort[i - 1 + M]; //i-1なのはA_sortが0-indexedであるため
         if (current_mL >= X) {
-            ans_cap = Math.min(ans_cap, i + M); // 水カップを全て飲み干していないと、日本酒のカップである保証がないから.
-
+            ans_cap = i + M; // 水カップを全て飲み干していないと、日本酒のカップである保証がないから.
+            break;
         }
     }
-    if (ans_cap == Infinity) {
-        console.log(-1);
-    } else {
-        console.log(ans_cap);
-    }
+    console.log(ans_cap);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
